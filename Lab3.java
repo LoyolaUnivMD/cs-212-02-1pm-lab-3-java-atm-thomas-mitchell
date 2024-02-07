3712a8a96d638e38d8984572f9c27e6a141c4c69
Trichards25 <131497949+Trichards25@users.noreply.github.com>
2024-02-07 14:48:14 -0500

    Added Comments

--- Lab3.java
+++ Lab3.java
@@ -1,134 +1,160 @@
 // Programmers:  Michel & Thomas
 // Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Due Date: 2/11/24
+// Lab Assignment: 3
+// Problem Statement: a program that acts at a smart ATM.
+// Data In: choices, deposit amount, withdraw amount
+// Data Out: account balance
+// Credits: input check I based off code I made in highschool
 
+//import decimalformat, inputMismatchException, Scanner
 import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 class Lab3 {
+    //makes a new public scanner called input
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        //makes a new format for displaying money
         DecimalFormat moneyFMT = new DecimalFormat("$,###.##");
 
         // Initialize variables
         double balance = 212.90;
+        //get input for name
         System.out.print("What is your name");
         String name = input.next();
+        //welcome message
         System.out.println("Welcome " + name);
 
-        int choice = intInputLimit(4, "What would you like to do: \n1.Deposit\n2.Withdraw\n3.Check balance\n4.Leave");
+        //get input for choice using intInput function
+        int choice = intInput(true,4, "What would you like to do: \n1.Deposit\n2.Withdraw\n3.Check balance\n4.Leave");
+        // loop while choice does not equal leave
         while (choice != 4) {
 
+            //if choice is equal to 1
             if (choice == 1) {
-                double deposit = doubleInput("How much money do you want to deposit? ");
+                //get input for how much to deposit using the doubleInput function
+                double deposit = doubleInput(false, 0.0,"How much money do you want to deposit? ");
+                //add the deposit amount to the balance
                 balance += deposit;
 
+                //if choice is equal to 2
             } else if (choice == 2) {
-                double withdraw = doubleInputLimit(balance,"How much money do you want to withdraw? ");
+                // get input for how much to withdraw using doubleInput function
+                double withdraw = doubleInput(true, balance, "How much money do you want to withdraw? ");
+                //subtract the withdraw amount from the balance
                 balance -= withdraw;
 
+                //if choice is equal to 3
             } else if (choice == 3) {
+                //output the balance and format it
                 System.out.println("Current Balance: " + moneyFMT.format(balance));
+                //if the choice does not equal any of the options output invalid choice
             } else {
                 System.out.println("Invalid Choice");
             }
-            choice = intInputLimit(4, "What would you like to do: \n1.Deposit\n2.Withdraw\n3.Check balance\n4.Leave");
+            //get input for choice again using intInput function
+            choice = intInput(true,4, "What would you like to do: \n1.Deposit\n2.Withdraw\n3.Check balance\n4.Leave");
 
         }
-        choice = intInputLimit(2, "Do you want your receipt printed or emailed? (1.print or 2.email)");
+        //get input for recipt choice using intInput function
+        choice = intInput(true, 2, "Do you want your receipt printed or emailed? (1.print or 2.email)");
+        //output thank you message
         System.out.println("Thank you for using the ATM");
+        //if the user inputted 1 output to not forget their recipt
         if (choice == 1) {
             System.out.println("Do not forget to take your recipt.");
-        }else{
+            //otherwise output to check their email for the recipt
+        } else {
             System.out.println("Check your email for your recipt");
         }
 
     }
 
-    public static int intInputLimit(int limit, String prompt) {
+    //function to check if an int input is valid and under a limit if useLimit is true
+    public static int intInput(boolean useLimit, int limit, String prompt) {
+        //initialize the variables
         int num = 0;
         boolean valid = false;
+        //loops until the input is valid
         while (!valid) {
+            //outputs the prompt
             System.out.println(prompt);
+            //tries to get the input and set as an int
             try {
                 num = input.nextInt();
-                if (num <= limit && num > 0) {
-                    valid = true;
+                //checks if we should check for a limit or not
+                if (useLimit) {
+                    // if the inputted num is less than the limit and greater than 0
+                    if (num <= limit && num > 0) {
+                        //sets valid to true which ends the loop
+                        valid = true;
+                        //otherwise outputs invalid choice
+                    } else {
+                        System.out.println("Invalid choice ");
+                    }
+                    //if not, just check if the num is over 0
                 } else {
-                    System.out.println("Invalid choice ");
+                    if (num > 0) {
+                        //sets valid to true which ends the loop
+                        valid = true;
+                        //otherwise outputs invalid choice
+                    } else {
+                        System.out.println("Invalid choice ");
+                    }
                 }
+                //if the program gets an error output invalid input and try again
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input ");
                 input.next();
             }
         }
+        //returns a valid num between the limit and 0
         return num;
     }
-    public static int intInput(String prompt) {
-        int num = 0;
-        boolean valid = false;
-        while (!valid) {
-            System.out.println(prompt);
-            try {
-                num = input.nextInt();
-                if (num > 0) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid choice ");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input ");
-                input.next();
-            }
-        }
-        return num;
-    }
-    public static double doubleInputLimit(Double limit, String prompt) {
+    //function to check if an double input is valid and under a limit if useLimit is true
+    public static double doubleInput(boolean useLimit, Double limit, String prompt) {
+        //initialize the variables
         double num = 0;
         boolean valid = false;
+        //loops until the input is valid
         while (!valid) {
+            //outputs the prompt
             System.out.println(prompt);
+            //tries to get the input and set as an double
             try {
                 num = input.nextDouble();
-                if (num > 0 && num <= limit) {
-                    valid = true;
+                //checks if we should check for a limit or not
+                if (useLimit) {
+                    // if the inputted num is less than the limit and greater than 0
+                    if (num > 0 && num <= limit) {
+                        //sets valid to true which ends the loop
+                        valid = true;
+                    } else {
+                        //otherwise outputs invalid choice
+                        System.out.println("Invalid choice ");
+                    }
+                    //if not, just check if the num is over 0
                 } else {
-                    System.out.println("Invalid choice ");
+                    if (num > 0) {
+                        //sets valid to true which ends the loop
+                        valid = true;
+                    } else {
+                        //otherwise outputs invalid choice
+                        System.out.println("Invalid choice ");
+                    }
                 }
+                //if the program gets an error output invalid input and trys again
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input ");
                 input.next();
             }
         }
-        return num;
-    }
-    public static double doubleInput(String prompt) {
-        double num = 0;
-        boolean valid = false;
-        while (!valid) {
-            System.out.println(prompt);
-            try {
-                num = input.nextDouble();
-                if (num > 0) {
-                    valid = true;
-                } else {
-                    System.out.println("Invalid choice ");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input ");
-                input.next();
-            }
-        }
+        //returns a valid num
         return num;
     }
 }
+
