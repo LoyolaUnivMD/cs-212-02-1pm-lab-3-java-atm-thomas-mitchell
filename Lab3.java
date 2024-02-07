90a660d227a5a42baf474a4b1c3fb21eec4d100f
Michele Onton <maontoncueva@loyola.edu>
2024-02-07 13:34:59 -0500

    lab3

--- Lab3.java
+++ Lab3.java
@@ -16,11 +16,8 @@ import java.util.SortedMap;
 class Lab3 {
     public static void main(String[] args) {
 
-        DecimalFormat moneyFMT = new DecimalFormat("$,###.##");
-
        // Initialize variables
         double balance = 212.90;
-
 
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name");
@@ -30,27 +27,29 @@ class Lab3 {
         System.out.println("What would you like to do: \nWithdraw\nCheck balance\nLeave");
         String choice = input.nextLine().strip().toLowerCase();
         while (!choice.equals("leave")) {
-           if (choice.equals("withdraw")){
 
-           }else if (choice.equals("check balance")){
-               System.out.println("Current Balance: "+ balance);
-           }else if (choice.equals("leave")) {
-                break;
+           if (choice.equals("deposit")) {
+               System.out.print("How much money do you want to deposit? ");
+               int deposit = input.nextInt();
+               balance += deposit;
+
+           } else if (choice.equals("withdraw")) {
+               System.out.print("How much money do you want to withdraw? ");
+               int withdraw = input.nextInt();
+               while 
+
+           } else if (choice.equals("check balance")){
+
+           } else if (choice.equals("leave")) {
+               System.out.println("Goodbye");
+               break;
+
            }else {
                System.out.println("Invalid Choice");
            }
-           if
-           System.out.println("What would you like to do: \nWithdraw\nCheck balance\nLeave");
+           System.out.println("What would you like to do: \nDeposit\nWithdraw\nCheck balance\nLeave");
            choice = input.nextLine().strip().toLowerCase();
 
-        }
-        System.out.println("Do you want your receipt printed or emailed? (print or email)");
-        String receipt = input.nextLine().strip().toLowerCase();
-        System.out.println("Thank you for using the ATM");
-        if (receipt.equals("print")){
-            System.out.println("Do not forget to take your recipt.");
-        }else if (receipt.equals("email")){
-            System.out.println("Check your email for your recipt");
         }
 
     }
