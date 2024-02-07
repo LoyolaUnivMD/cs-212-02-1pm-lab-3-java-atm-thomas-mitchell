9cae13984d8567ddb09a8ed3419118b49189ea90
Trichards25 <131497949+Trichards25@users.noreply.github.com>
2024-02-07 13:32:58 -0500

    loop

--- Lab3.java
+++ Lab3.java
@@ -16,8 +16,11 @@ import java.util.SortedMap;
 class Lab3 {
     public static void main(String[] args) {
 
+        DecimalFormat moneyFMT = new DecimalFormat("$,###.##");
+
        // Initialize variables
         double balance = 212.90;
+
 
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name");
@@ -30,17 +33,24 @@ class Lab3 {
            if (choice.equals("withdraw")){
 
            }else if (choice.equals("check balance")){
-
+               System.out.println("Current Balance: "+ balance);
            }else if (choice.equals("leave")) {
-               System.out.println("Goodbye");
-               break;
-
+                break;
            }else {
                System.out.println("Invalid Choice");
            }
+           if
            System.out.println("What would you like to do: \nWithdraw\nCheck balance\nLeave");
            choice = input.nextLine().strip().toLowerCase();
 
+        }
+        System.out.println("Do you want your receipt printed or emailed? (print or email)");
+        String receipt = input.nextLine().strip().toLowerCase();
+        System.out.println("Thank you for using the ATM");
+        if (receipt.equals("print")){
+            System.out.println("Do not forget to take your recipt.");
+        }else if (receipt.equals("email")){
+            System.out.println("Check your email for your recipt");
         }
 
     }
